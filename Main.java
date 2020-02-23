6def99867a8295de8035b65063263b0fd7522fec
samuel <samuelcadiou@hotmail.fr>
2020-02-23 15:48:16 +0100

    Add first view + set up MVC

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,8 +9,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.out.println("Programm Starting");
-        
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("view/FirstWindow.fxml"));
         primaryStage.setTitle("GSB Application");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
